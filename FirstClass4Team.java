e94588310082d68d929caab457e819b2270bc607
A7144888 <01257161@email.ntou.edu.tw>
2025-04-27 23:27:04 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,7 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257147\"\n姓名：\"吳致緯\"");
         System.out.println("\"學號：\"01257062\"\n姓名：\"陳彥中\"");
+        System.out.println("\"學號：\"01257161\"\n姓名：\"李永聖\"");
+
     }
 }
