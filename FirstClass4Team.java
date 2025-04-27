9c9615b93da6ef446553728b3281f2f26eecb37a
zzmthd <151005868+zzmthd@users.noreply.github.com>
2025-04-27 21:44:41 +0800

    Add print second memeber name

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257147\"\n姓名：\"吳致緯\"");
+        System.out.println("\"學號：\"01257062\"\n姓名：\"陳彥中\"");
     }
 }
